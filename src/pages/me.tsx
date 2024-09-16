026289ada4c3b8feed9f34d5c6c038e7c299ddc3
Kim Avillanosa <avillanosakzxc@gmail.com>
2024-09-16 17:24:34 +0000

    revised to production build

--- src/pages/me.tsx
+++ src/pages/me.tsx
@@ -1,12 +1,9 @@
 import Page from "@/shared/components/layout/Page";
 import Layout from "@/shared/components/layout/Layout";
 import { NextPage } from "next";
-import LargeHeadline from "@/shared/components/LargeHeadline";
 import Headline from "@/shared/modules/home/Headline";
 import QuickBackground from "@/shared/modules/home/QuickBackground";
-import WorkProjects from "@/shared/modules/home/Projects";
 import TechStack from "@/shared/modules/home/Stack";
-import ForFun from "@/shared/modules/home/ForFun";
 
 const Me: NextPage = () => {
     return (
